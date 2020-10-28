f3fe746e363921abf3a1ba383a9dffdccaee4451
Kieran Klaassen <kieranklaassen@gmail.com>
2020-10-28 12:57:37 -0500

    chore: cleanup readme

--- lib/moneytree/account.rb
+++ lib/moneytree/account.rb
@@ -3,6 +3,7 @@ require 'active_support/concern'
 module Moneytree
   module Account
     extend ActiveSupport::Concern
+    # FIXME: see if we can remove  config.current_account = :current_merchant and set it from here
 
     included do
       has_one :moneytree_payment_gateway, class_name: 'Moneytree::PaymentGateway', foreign_key: 'account_id', inverse_of: :account, as: :account

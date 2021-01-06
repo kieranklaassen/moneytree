06219530a2497e98d8334333d7b699927bd8d08e
Pelle ten Cate <pelletencate@gmail.com>
2021-01-06 22:43:46 +0100

    fix: remove incorrect `transaction`

--- app/models/moneytree/transaction.rb
+++ app/models/moneytree/transaction.rb
@@ -58,7 +58,7 @@ module Moneytree
       end
 
       if Moneytree.order_status_trigger_method && saved_change_to_status?
-        transaction.order.send(Moneytree.order_status_trigger_method, transaction)
+        order.send(Moneytree.order_status_trigger_method, transaction)
       end
     end
   end
